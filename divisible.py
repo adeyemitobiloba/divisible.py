368dc4155db8540650c737bfbcea32ed22dbb038
adeyemitobiloba <tobilobaadeyemi400@gmail.com>
2024-07-06 23:04:39 +0100

    divisible.py

---
+++ divisible.py
@@ -0,0 +1,11 @@
+integers=int(input("enter your answer:"))
+the_numbers=int("8")
+the_values=int("6")
+if integers==the_numbers:
+    print("15//2")
+elif integers>the_numbers:
+    print("15//2")
+elif integers<the_values:
+    print("try again")
+else:
+    print("i think you are to smart")            
